adc75516e460e159e0fb5bf00e97ddebc762b026
scientificworld <scientificworld@126.com>
2020-08-13 23:13:04 +0800

    .

--- code/infu/repl/repl.hpp
+++ code/infu/repl/repl.hpp
@@ -6,8 +6,9 @@ Part of The Infinity Universe Programming Language Project, Version 1.0.
 
 #include <iostream>
 #include <string>
-//#include "../lang/interpret.hpp"
-#include "../../slib/color.hpp"
+#include "../lang/interpret.hpp"
+// #include "../../slib/color.hpp"
+#include "../../slib/part.hpp"
 
 #ifndef _INFU_REPL_REPL_
 #define _INFU_REPL_REPL_
@@ -16,11 +17,21 @@ namespace infu {
 	namespace repl {
 		int repl() {
 			int _bra = 0;
+			std::string _inp, _line;
 			while(1) {
-				std::string _inp;
 				std::cout << "infu > ";
 				std::getline(std::cin, _inp);
-				
+				// _part = space::part_left(_inp);
+				_bra += space::part_left(_inp);
+				while(_bra != 0) {
+					_inp += '\n';
+					std::cout << "infu >> ";
+					std::getline(std::cin, _line);
+					_bra += space::part_left(_line);
+					_inp += _line;
+				}
+				std::cout << _inp << std::endl;
+				infu::lang::interpret(_inp);
 			}
 		}
 		/*
--- code/slib/part.hpp
+++ code/slib/part.hpp
@@ -5,13 +5,35 @@
 */
 
 #include <string>
+#include <algorithm>
 
 #ifndef _SLIB_PART_HPP_
 #define _SLIB_PART_HPP_
 
-namespace space {
-	void part(std::string a,int b,char c='(',char d=')') {
-		// TODO: more arguments
+/*namespace _space {
+}*/
 
+namespace space {
+	int part_match(std::string a,int b=0,char c='(',char d=')') {
+		// TODO: more arguments
+	}
+	int part_left(std::string x,char y='{',char z='}') {
+		// Impl 1
+		int w=0;
+		for(int u=0;u<x.size();u++) {
+			char v=x[u];
+			if(v==y) w++;
+			if(v==z) w--;
+		}
+		/*if(w<0) return -1;
+		else */ return w;
+	}
+	int _part_left(std::string x,char y='{',char z='}') {
+		// Impl 2, use std::count
+		int w=std::count(x.begin(),x.end(),y)-std::count(x.begin(),x.end(),z);
+		/* if(w<0) return -1;
+		else */ return w;
 	}
 }
+
+#endif
