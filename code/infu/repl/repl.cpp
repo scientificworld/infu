e56db7dc84ea75fdc10a20247a6496a1163b7d1b
scientificworld <scientificworld@126.com>
2020-08-13 14:17:55 +0800

    .

---
+++ code/infu/repl/repl.cpp
@@ -0,0 +1,16 @@
+/*
+
+Part of The Infinity Universe Programming Language Project, Version 1.0.
+
+*/
+
+#include "repl.hpp"
+
+#ifndef _INFU_REPL_
+#define _INFU_REPL_
+
+int main() {
+	return infu::repl::repl();
+}
+
+#endif
--- code/infu/repl/repl.hpp
+++ code/infu/repl/repl.hpp
@@ -6,14 +6,28 @@ Part of The Infinity Universe Programming Language Project, Version 1.0.
 
 #include <iostream>
 #include <string>
+//#include "../lang/interpret.hpp"
+#include "../../slib/color.hpp"
 
 #ifndef _INFU_REPL_REPL_
 #define _INFU_REPL_REPL_
 
 namespace infu {
 	namespace repl {
-		void repl() {
-			
+		int repl() {
+			int _bra = 0;
+			while(1) {
+				std::string _inp;
+				std::cout << "infu > ";
+				std::getline(std::cin, _inp);
+				
+			}
 		}
+		/*
+		void repl() {
+		}
+		*/
 	}
 }
+
+#endif
