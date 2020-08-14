bb0e5a0bf9df49f7c7e9b0f73c9f86a166c81c5c
scientificworld <scientificworld@126.com>
2020-08-14 08:58:05 +0800

    .

---
+++ code/infu/lang/interpret.hpp
@@ -0,0 +1,20 @@
+/*
+
+Part of The Infinity Universe Programming Language Project, Version 1.0.
+
+*/
+
+#include <iostream>
+#include <string>
+
+#ifndef _INFU_LANG_INTERPRET_
+#define _INFU_LANG_INTERPRET_
+
+namespace infu {
+	namespace lang {
+		void interpret(std::string _prog) {
+		}
+	}
+}
+
+#endif
